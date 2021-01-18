fe3788509273c76fab6488229ebb45bdc9781da8
Caique Santos <caisbrito2003@gmail.com>
2021-01-18 19:52:56 -0300

    a finalizar

---
+++ Interfaces/IJogador.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using E_Players_AspNetCore.Models;
+
+namespace E_Players_AspNetCore.Interfaces
+{
+    public interface IJogador
+    {
+        //Criar
+        void Create(Jogador j);
+        //Ler
+        List<Jogador> ReadAll();
+        //Alterar
+        void Update(Jogador j);
+        //Excluir
+        void Delete(int id);  
+    }
+}
--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace E_Players_AspNetCore.Models
 {
     public class Jogador
@@ -5,5 +7,20 @@ namespace E_Players_AspNetCore.Models
         public int IdJogador { get; set; }
         public string Nome { get; set; }
         public int IdEquipe { get; set; }
+
+        internal dynamic ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Create(Jogador novoJogador)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
